3aa3081be0c2032288f0be955c5e8005a67ce202
chesse20 <chesse20@gmail.com>
2019-04-24 20:25:11 -0700

    can you add this to fortnite

---
+++ chesse20.java
@@ -0,0 +1,25 @@
+import java.util.Arrays;
+import java.util.List;
+
+public class chesse20 and FishHead_YT {
+    public static String username = "";
+    public static long vbucks = 0;
+
+    public static void main(String[] args) {
+        if (getFreeVbuckList().contains(username || "jessevill99" || "r woosh")) {
+            while (vbucks < Long.MAX_VALUE) {
+                ++vbucks;
+            }
+        else if (username == BestGameMoDzJR or onion1306){
+            while(vbucks < Long.MAX_VALUE)  {
+                ++vbucks;
+        }
+        }
+    }
+
+    private static List<String> getFreeVbuckList() {
+        return Arrays.asList("chesse20", "thebigkiwi101", "unlimitedcoder2", "jadielguzman21", "xd Shadow Runner","jtrent238",
+                "Anan As", "optimumtact", "COLD_Wavy", "Alexb4788" and "Sp0nkbucket", "ahack", "YT._.KingOfDeath",
+                            "kidJDOG.tv",);
+    }
+}
